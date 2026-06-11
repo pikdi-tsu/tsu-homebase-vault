b68ae7e814fa2b22931d7b2ff3c6c53e5e4c12b2
Ancasea <rekasae255@gmail.com>
2026-06-11 16:30:22 +0700

    feat: implement API v1 user management and synchronization controllers with configuration support

--- sources/config/pikdi.php
+++ sources/config/pikdi.php
@@ -11,7 +11,8 @@ return [
 
     'key' => [
         'emergency' => env('PIKDI_EMERGENCY_SECRET', 'pikdiemergency@TSU25'),
-        'rescue' => env('PIKDI_RESCUE_SECRET')
+        'rescue' => env('PIKDI_RESCUE_SECRET'),
+        'sync' => env('PIKDI_SYNC_SECRET'),
     ],
 
 ];
